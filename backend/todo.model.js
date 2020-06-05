ee78148a0d46c535e1ae3cfee933c0797be3368a
chattha926242 <41201474+chattha926242@users.noreply.github.com>
2020-06-05 22:20:02 +0500

    First Commit

---
+++ backend/todo.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Todo = new Schema({
+    todo_description: {
+        type: String
+    },
+    todo_date: {
+        type: Date
+    },
+    todo_catagory: {
+        type: String
+    },
+    todo_responsible: {
+        type: String
+    },
+    todo_priority: {
+        type: String
+    },
+    todo_completed: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('Todo', Todo);
